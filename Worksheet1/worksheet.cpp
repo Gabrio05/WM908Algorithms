d50af6095fff29900e0313d66627b459eab4c461
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2025-10-13 11:13:19 +0100

    Add birthday function

--- Worksheet1/worksheet.cpp
+++ Worksheet1/worksheet.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void birthday() {
+	int day = 0;
+	int month = 0;
+	while (day < 1 || day > 31) {
+		std::cout << "Help! Quickly! What is your day of birth? (Number between 1 and 31) ";
+		std::cin >> day;
+	}
+	while (month < 1 || month > 12) {
+		std::cout << "OK! Now the month! Quick! (Number between 1 and 12) ";
+		std::cin >> month;
+	}
+}
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	birthday();
 	return 0;
 }
