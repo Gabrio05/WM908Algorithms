79abe3145273ad9042cebb707d70ba0e50d65483
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2025-10-16 17:12:44 +0100

    Add Camera Class Basics and clean code

---
+++ AlgorithmsGame/GameParameters.h
@@ -0,0 +1,15 @@
+#ifndef GAMEPARAMETERS
+#define GAMEPARAMETERS
+
+namespace GameParameters {
+
+	int window_width = 1024;
+	int window_height = 768;
+
+	namespace Camera {
+		float acceleration = 2.0f;
+	}
+
+}
+
+#endif
--- AlgorithmsGame/MainLoop.cpp
+++ AlgorithmsGame/MainLoop.cpp
@@ -1,20 +1,41 @@
 #include "GameBase/GamesEngineeringBase.h"
-#define WIDTH 1024
-#define HEIGHT 768
+#include "GameParameters.h"
+ 
+class Camera {
+	int x_position;
+	int y_position;
+	float x_velocity;
+	float y_velocity;
+	const float acceleration = GameParameters::Camera::acceleration;
+public:
+	// Update camera position smoothly based on objective destination
+	void update_position(int x, int y, float delta) {
+
+	}
+	int get_x() { return (int)(x_position + 0; }
+	int get_y() { return y_position; }
+};
+
+void drawImage(GamesEngineeringBase::Window &canvas) {
+	// Background -> Effects (if any) -> Hero -> Enemies -> Hero Projectiles -> Enemy Projectiles
+	// Background
+	for (int i = 0; i < GameParameters::window_height; i++) {
+		for (int j = 0; j < GameParameters::window_width; j++) {
+			if (i % 50 == 0 && j % 50 == 0) {
+				canvas.draw(j, i, 255, 255, 128);
+			}
+		}
+	}
+}
 
 int main() {
 	GamesEngineeringBase::Window canvas;
-	canvas.create(WIDTH, HEIGHT, "Test");
+	canvas.create(GameParameters::window_width, GameParameters::window_height, "Test");
 	bool isRunning = true;
 
 	while (isRunning) {
 		canvas.clear();
-		for (int i = 0; i < WIDTH; i++) {
-			for (int j = 0; j < HEIGHT; j++) {
-				canvas.draw(i, j, 126, 0, 0);
-				canvas.draw(i, j, 126, 0, 0);
-			}
-		}
+		drawImage(canvas);
 		canvas.present();
 	}
 	return 0;
